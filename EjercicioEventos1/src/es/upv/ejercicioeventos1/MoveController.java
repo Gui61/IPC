777819d3e55075a19294de3ddfdcd73f7929cddb
Gui61 <guitormo@gmail.com>
2015-02-20 14:42:26 +0100

    IPC S2

---
+++ EjercicioEventos1/src/es/upv/ejercicioeventos1/MoveController.java
@@ -0,0 +1,12 @@
+package es.upv.ejercicioeventos1;
+
+import javafx.scene.input.KeyEvent;
+
+public class MoveController {
+	@FXML
+	private Button myButton;
+	
+	@FXML
+	protected void move(KeyEvent event) {
+		
+}
