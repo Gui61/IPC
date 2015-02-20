19d0759f4157d84edfc0cf6a30d1d20f160e9a48
Gui61 <guitormo@gmail.com>
2015-02-20 20:28:53 +0100

    IPC incompleto

--- EjercicioEventos1/src/es/upv/ejercicioeventos1/MoveController.java
+++ EjercicioEventos1/src/es/upv/ejercicioeventos1/MoveController.java
@@ -1,12 +1,24 @@
 package es.upv.ejercicioeventos1;
 
+import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 public class MoveController {
 	@FXML
-	private Button myButton;
+	private HBox myButton;
 	
 	@FXML
-	protected void move(KeyEvent event) {
-		
+	protected void move(KeyEvent keyEvent) {
+		KeyCode pressedKey = keyEvent.getCode();
+		if(KeyCode.UP == pressedKey){
+	        GridPane.setHalignment(HBox,HPos.RIGHT);
+
+		}
+			
+		}
 }
