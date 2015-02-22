c057e06f23c1f3b0cb950f5c58afdfccbc8a3b60
Gui61 <guitormo@gmail.com>
2015-02-22 23:56:43 +0100

    P2

--- EjercicioEventos1/src/es/upv/ejercicioeventos1/SampleController.java
+++ EjercicioEventos1/src/es/upv/ejercicioeventos1/SampleController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import java.util.*;
 
 public class SampleController {
 	@FXML
@@ -15,6 +16,7 @@ public class SampleController {
 		final int x = grid.getColumnIndex(boton);
 		final int y = grid.getRowIndex(boton);
 		switch(key.getCode()){
+		case W:
 		case UP:
 			if(y>0){
 				grid.setRowIndex(boton, y-1);
@@ -22,23 +24,30 @@ public class SampleController {
 			else{
 				grid.setRowIndex(boton, 4);
 			break;}
+		case S:
 		case DOWN:
 			if(y<4){
 				grid.setRowIndex(boton, y+1);}
 			else{
 				grid.setRowIndex(boton, 0);}
 			break;
+		case A:
 		case LEFT:
 			if(x>0){
 				grid.setColumnIndex(boton, x-1);}
 			else{
 				grid.setColumnIndex(boton, 4);}
 			break;
+		case D:
 		case RIGHT:
 			if(x<4){
 				grid.setColumnIndex(boton, x+1);}
 			else{
 				grid.setColumnIndex(boton, 0);}
+			break;
+		case ENTER:
+			grid.setRowIndex(boton, (int) (Math.random()*5));
+			grid.setColumnIndex(boton, (int) (Math.random()*5));
 			break;
 		default:
 			break;
