96e0ba25fe90179ff5ab6e3507295b2ade505e9b
Gui61 <guitormo@gmail.com>
2015-02-22 22:28:13 +0100

    P2 funciona mas o menos

--- EjercicioEventos1/src/es/upv/ejercicioeventos1/SampleController.java
+++ EjercicioEventos1/src/es/upv/ejercicioeventos1/SampleController.java
@@ -2,16 +2,46 @@ package es.upv.ejercicioeventos1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 
 public class SampleController {
 	@FXML
 	private Button boton;
+	private GridPane grid;
 	
 	@FXML
-	protected void move(KeyEvent key) {
-		if(key.getCode()== KeyCode.UP)
-			System.out.print("asda");
+	protected void botonMove(KeyEvent key) {
+		final int x = grid.getColumnIndex(boton);
+		final int y = grid.getRowIndex(boton);
+		switch(key.getCode()){
+		case UP:
+			if(y>0){
+				grid.setRowIndex(boton, y-1);
+				break;}
+			else{
+				grid.setRowIndex(boton, 4);
+			break;}
+		case DOWN:
+			if(y<4){
+				grid.setRowIndex(boton, y+1);}
+			else{
+				grid.setRowIndex(boton, 0);}
+			break;
+		case LEFT:
+			if(x>0){
+				grid.setColumnIndex(boton, x-1);}
+			else{
+				grid.setColumnIndex(boton, 4);}
+			break;
+		case RIGHT:
+			if(x<4){
+				grid.setColumnIndex(boton, x+1);}
+			else{
+				grid.setColumnIndex(boton, 0);}
+			break;
+		default:
+			break;
+		}			
 	}
 }
