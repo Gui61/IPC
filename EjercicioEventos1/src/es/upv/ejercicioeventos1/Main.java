1d9bfc94b280cd95766bc374a3978d879e02068c
Gui61 <guitormo@gmail.com>
2015-02-27 15:00:57 +0100

    casi rebien

--- EjercicioEventos1/src/es/upv/ejercicioeventos1/Main.java
+++ EjercicioEventos1/src/es/upv/ejercicioeventos1/Main.java
@@ -1,6 +1,8 @@
 package es.upv.ejercicioeventos1;
 	
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Scene;
@@ -13,11 +15,25 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("/es/upv/ejercicioeventos1/view/GridPane.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Cuadrados locos");
 			primaryStage.setScene(scene);
 			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			
+			final DoubleProperty x= new SimpleDoubleProperty();
+			final DoubleProperty y= new SimpleDoubleProperty();
+			
+			scene.setOnMousePressed((e)->{
+				x.setValue(primaryStage.getX() - e.getScreenX());
+				y.setValue(primaryStage.getY() - e.getScreenY());
+			});
+			
+			scene.setOnMouseDragged((e)->{
+				primaryStage.setX(e.getScreenX() + x.getValue());
+				primaryStage.setY(e.getScreenY() + y.getValue());
+			});
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
