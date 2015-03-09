5bf88dfffb2a3a2c7c29abbdd1d48d0a64b08a46
Gui61 <guitormo@gmail.com>
2015-03-09 14:50:06 +0100

    9-3

--- Calculadora/src/calculhadora/CalcController.java
+++ Calculadora/src/calculhadora/CalcController.java
@@ -1,10 +1,6 @@
 package calculhadora;
 
-import javafx.beans.InvalidationListener;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -12,55 +8,62 @@ import javafx.scene.control.TextArea;
 
 public class CalcController {
 	@FXML
-	private Button num0;
-	@FXML
-	private Button num1;
-	@FXML
-	private Button num2;
-	@FXML
-	private Button num3;
-	@FXML
-	private Button num4;
-	@FXML
-	private Button num5;
-	@FXML
-	private Button num6;
-	@FXML
-	private Button num7;
-	@FXML
-	private Button num8;
-	@FXML
-	private Button num9;
-	@FXML
-	private Button suma;
-	@FXML
-	private Button resta;
-	@FXML
-	private Button mult;
-	@FXML
-	private Button div;
-	@FXML
-	private Button igual;
-	@FXML
-	private Button clear;
-	@FXML
-	private Button coma;
-	@FXML
 	private TextArea res;
-	
+	protected String valor = "";
+	protected String stackValor = "0";
+	protected String op="";	
 	@FXML
-	protected DoubleProperty operando = new SimpleDoubleProperty(0.0);
-		op1;
-		op2;
-		opDec;
-		op;
-
-	@FXML
-	protected void write(ActionEvent e){
-		
+	protected void digitos(ActionEvent e){
+		Button botonPulsado = (Button) e.getTarget();
+		String botonString = botonPulsado.getText();
+		valor += botonString;
+		res.setText(valor);
 	}
 	@FXML
 	protected void op(ActionEvent e){
-		
+		Button botonPulsado = (Button) e.getTarget();
+		String botonString = botonPulsado.getText();
+		switch(botonString){
+		case "C":
+			valor = "";
+			res.setText("");
+			stackValor ="0";
+		case "+":
+			stackValor = res.getText();
+			res.setText("");
+			valor = "";
+			op = "+";
+		case "-":
+			stackValor = res.getText();
+			res.setText("");
+			valor = "";
+			op = "-";
+		case "X":
+			stackValor = res.getText();
+			res.setText("");
+			valor = "";
+			op = "X";
+		case "/":
+			stackValor = res.getText();
+			res.setText("");
+			valor = "";
+			op = "/";
+		case ".":
+			valor += "+";
+		case "=":
+			double x = Double.parseDouble(stackValor);
+			double y = Double.parseDouble(valor);
+			switch(op){
+			case "+":
+				res.setText(String.valueOf(x+y));
+			case "-":
+				res.setText(String.valueOf(x-y));
+			case "X":
+				res.setText(String.valueOf(x*y));
+			case "/":
+				res.setText(String.valueOf(x/y));
+			}
+		}
 	}
+
 }
--- Calculadora/src/calculhadora/Main.java
+++ Calculadora/src/calculhadora/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("es.upv.calc.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setResizable(false);
 			primaryStage.setScene(scene);
